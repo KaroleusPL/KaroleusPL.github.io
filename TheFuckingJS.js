2423a007b3ad26b7ef9bc1cef235aef7fe363483
Karoleus.PL <130451476+KaroleusPL@users.noreply.github.com>
2024-09-07 23:46:49 +0200

    Drip 100%

--- TheFuckingJS.js
+++ TheFuckingJS.js
@@ -1,9 +1,14 @@
 const audio = new Audio('Sounds/WatrobaDrip.mp3')
 
 window.addEventListener("load", function() {
-  const watroba = document.getElementById("MrWatroba");
+    const watroba = document.getElementById("MrWatroba");
+    const Jumpscare = document.getElementById("MrWatrobaJumpscare");
 
-  watroba.addEventListener("click", function() {
-    audio.play();
-  });
+    watroba.addEventListener("click", function() {
+        Jumpscare.style.display = "block";
+        audio.play();
+        setTimeout(function() {
+            Jumpscare.style.display = "none";
+          }, 7000)
+    });
 });
