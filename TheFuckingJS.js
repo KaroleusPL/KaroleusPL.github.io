113c0977c71e3efdd1ab8a35f14e29f5a6b9759f
Wolfyxon <wolfyxon@gmail.com>
2024-09-08 21:03:21 +0200

    Using lowercase names

--- TheFuckingJS.js
+++ TheFuckingJS.js
@@ -2,14 +2,14 @@ const audio = new Audio('Sounds/WatrobaDrip.mp3')
 
 window.addEventListener("load", () => {
     const watroba = document.getElementById("MrWatroba");
-    const Jumpscare = document.getElementById("MrWatrobaJumpscare");
+    const jumpscare = document.getElementById("MrWatrobaJumpscare");
 
     watroba.addEventListener("click", () => {
         Jumpscare.style.display = "block";
         audio.currentTime = 0;
         audio.play();
         setTimeout(function() {
-            Jumpscare.style.display = "none";
+            jumpscare.style.display = "none";
         }, 7000)
     });
 });
